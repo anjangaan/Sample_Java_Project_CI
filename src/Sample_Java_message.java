c677c7bee5eb2ff323b482e235f7ddc3cfa027e6
anjangaan <gaan.anjan@gmail.com>
2018-11-14 11:18:50 -0800

    Initial commit

---
+++ src/Sample_Java_message.java
@@ -0,0 +1,8 @@
+import java.time.LocalDateTime;
+
+public class Sample_Java_message {
+	public static void main(String[] args) {
+        LocalDateTime current = LocalDateTime.now();
+        System.out.println("Current Date and Time is: " + current);
+    }
+}
